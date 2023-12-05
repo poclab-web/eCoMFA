e43718f0c7c5bd8f059f3adbf7c5e3454d647ae0
shimonomasakiynu <shimono-masaki-rb@ynu.jp>
2023-12-05 15:54:32 +0900

    1205 make_ΔΔGhist完成

---
+++ CoMFA_model_lib/make_ΔΔGhist.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+df1 = pd.read_excel("../arranged_dataset/cbs.xls").dropna(subset=['smiles']).drop_duplicates(subset="inchikey")
+df2=pd.read_excel("../arranged_dataset/DIP-chloride.xls").dropna(subset=['smiles']).drop_duplicates(subset="inchikey")
+df3 =pd.read_excel("../arranged_dataset/Russ.xls").dropna(subset=['smiles']).drop_duplicates(subset="inchikey")
+for (df,name) in zip([df1,df2,df3],["cbs","dip","Russ"]):
+
+
+    plt.hist(df["er."])
+
+    os.makedirs("../datahist",exist_ok=True)
+    plt.savefig("../datahist"+"/{}erplot.png".format(name))
+    plt.clf()
+
+for (df, name) in zip([df1, df2, df3], ["cbs", "dip", "Russ"]):
+    plt.hist(df["ΔΔG.expt."])
+
+
+    os.makedirs("../datahist", exist_ok=True)
+    plt.savefig("../datahist" + "/{}ΔΔGplot.png".format(name))
+    plt.clf()
+
