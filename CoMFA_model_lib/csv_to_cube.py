7ac7faff4dbb2e86e2b5d0227e8d77049235eefd
shimonomasakiynu <shimono-masaki-rb@ynu.jp>
2024-03-05 16:04:01 +0900

    20240305ver

--- CoMFA_model_lib/csv_to_cube.py
+++ CoMFA_model_lib/csv_to_cube.py
@@ -2,6 +2,7 @@ import copy
 import json
 import os
 import pandas as pd
+import glob
 
 
 # def fesp_deletenucleo(n, feature):
@@ -26,71 +27,48 @@ def mfunfolding(df):
     df_yz["z"] = -df_yz["z"]
     df_yz["MF_Dt"] = -df_yz["MF_Dt"]
     try:
-        df_z["MF_ESP_cutoff"] = -df_z["MF_ESP_cutoff"]
-        df_yz["MF_ESP_cutoff"] = -df_yz["MF_ESP_cutoff"]
+        try:
+            df_z["MF_ESP_cutoff"] = -df_z["MF_ESP_cutoff"]
+            df_yz["MF_ESP_cutoff"] = -df_yz["MF_ESP_cutoff"]
+        except:
+            df_z["MF_ESP"] = -df_z["MF_ESP"]
+            df_yz["MF_ESP"] = -df_yz["MF_ESP"]
     except:
-        df_z["MF_ESP"] = -df_z["MF_ESP"]
-        df_yz["MF_ESP"] = -df_yz["MF_ESP"]
+        None
 
     df = pd.concat([df, df_y, df_z, df_yz]).sort_values(by=["x", "y", "z"], ascending=[True, True, True])
-
     return df
 
+def zunfolding(df,regression_features):
+    df.to_csv("../errortest/dfbefore.csv")
+    df_z = copy.deepcopy(df)
+    df_z["z"]=-df_z["z"]
+    df_z["MF_Dt"]=-df_z["MF_Dt"]
+    df_z["MF_{}".format(regression_features.split()[1])]=-df_z["MF_{}".format(regression_features.split()[1])]
+    df = pd.concat([df ,df_z ]).sort_values(['x', 'y',"z"], ascending=[True, True,True])#.sort_values(by=["x", "y", "z"])
+    df.to_csv("../errortest/dfafter.csv")
+    return df
 
 if __name__ == '__main__':
-    for param_file_name in [
-        # "../parameter_nomax/parameter_cbs_gaussian.txt",
-        # "../parameter_nomax/parameter_cbs_gaussian.txt",
-        # "../parameter_nomax/parameter_cbs_ridgecv.txt",
-        # "../parameter_nomax/parameter_cbs_PLS.txt",
-        # "../parameter_nomax/parameter_cbs_lassocv.txt",
-        # "../parameter_nomax/parameter_cbs_elasticnetcv.txt",
-        # "../parameter_nomax/parameter_RuSS_gaussian.txt",
-        # "../parameter_nomax/parameter_RuSS_lassocv.txt",
-        # "../parameter_nomax/parameter_RuSS_PLS.txt",
-        # "../parameter_nomax/parameter_RuSS_elasticnetcv.txt",
-        # "../parameter_nomax/parameter_RuSS_ridgecv.txt",
-        # "../parameter_nomax/parameter_dip-chloride_PLS.txt",
-        # "../parameter_nomax/parameter_dip-chloride_lassocv.txt",
-        # "../parameter_nomax/parameter_dip-chloride_gaussian.txt",
-        # "../parameter_nomax/parameter_dip-chloride_elasticnetcv.txt",
-        # "../parameter_nomax/parameter_dip-chloride_ridgecv.txt",
-
-        "../parameter_0227/parameter_cbs_gaussian.txt",
-        "../parameter_0227/parameter_RuSS_gaussian.txt",
-        "../parameter_0227/parameter_dip-chloride_gaussian.txt",
-
-        # "../parameter_0207/parameter_cbs_gaussian.txt",
-        # "../parameter_0207/parameter_cbs_ridgecv.txt",
-        # "../parameter_0207/parameter_cbs_PLS.txt",
-        # "../parameter_0207/parameter_cbs_lassocv.txt",
-        # "../parameter_0207/parameter_cbs_elasticnetcv.txt",
-        # "../parameter_0227/parameter_RuSS_gaussian.txt",
-        # "../parameter_0207/parameter_RuSS_lassocv.txt",
-        # "../parameter_0207/parameter_RuSS_PLS.txt",
-        # "../parameter_0207/parameter_RuSS_elasticnetcv.txt",
-        # "../parameter_0207/parameter_RuSS_ridgecv.txt",
-        # "../parameter_0207/parameter_dip-chloride_PLS.txt",
-        # "../parameter_0207/parameter_dip-chloride_lassocv.txt",
-        # "../parameter_0227/parameter_dip-chloride_gaussian.txt",
-        # "../parameter_0207/parameter_dip-chloride_elasticnetcv.txt",
-        # "../parameter_0207/parameter_dip-chloride_ridgecv.txt",
-    ]:
-
-        print(param_file_name)
-        with open(param_file_name, "r") as f:
-            param = json.loads(f.read())
-        features_dir_name = param["moleculer_field_dir"]  # param["grid_dir_name"]
-
-        # "../cube_aligned_b3lyp_6-31g(d)/ADCYRBXQAJXJTD-UHFFFAOYSA-N/ESP02_0.cube"
-        # inchykey = "WRYKPYJMRHQDBM-UHFFFAOYSA-N","KWOLFJPFCHCOCG-UHFFFAOYSA-N"KWOLFJPFCHCOCG-UHFFFAOYSA-N　IMACFCSSMIZSPP-UHFFFAOYSA-N
-
-        # feature="MF_Dt"
-        fold = True
-        dir_name = param["moleculer_field_dir"]
-        os.makedirs(dir_name, exist_ok=True)
+    # for param_file_name in [
+    #     "../parameter_0227/parameter_cbs_gaussian.txt",
+    #     "../parameter_0227/parameter_RuSS_gaussian.txt",
+    #     "../parameter_0227/parameter_dip-chloride_gaussian.txt",
+    # ]:
+    for file in glob.glob("../result/{}".format("*")+"/molecular_filed{}.csv".format("*")):
+        df= pd.read_csv(file)
+        # with open(param_file_name, "r") as f:
+        #     param = json.loads(f.read())
+        # features_dir_name = param["moleculer_field_dir"]
+        # fold = True
+        # dir_name = param["moleculer_field_dir"]
+        # os.makedirs(dir_name, exist_ok=True)
         df = pd.read_csv(
-            dir_name + "/moleculer_field.csv")  # "{}/{}/feature_yz.csv".format(features_dir_name, inchykey))
+            dir_name + "/moleculer_field.csv")
+        df_unfold = df
+        df_unfold = mfunfolding(df_unfold)
+        df_unfold
+        # "{}/{}/feature_yz.csv".format(features_dir_name, inchykey))
         # for feature,cube_file_name in zip(["MF_Dt","MF_ESP","MF_ESP_cutoff"],["../cube_aligned_b3lyp_6-31g(d)/KWOLFJPFCHCOCG-UHFFFAOYSA-N/Dt02_0.cube",
         #                                                       "../cube_aligned_b3lyp_6-31g(d)/KWOLFJPFCHCOCG-UHFFFAOYSA-N/ESP02_0.cube",
         #                                                     "../cube_aligned_b3lyp_6-31g(d)/KWOLFJPFCHCOCG-UHFFFAOYSA-N/ESP02_0.cube"
@@ -104,41 +82,7 @@ if __name__ == '__main__':
         ]):
             df = pd.read_csv(dir_name + "/moleculer_field.csv")
             df = mfunfolding(df)
-            # if fold:
-            #     df = pd.read_csv(dir_name + "/moleculer_field.csv")
-            #     df_y=copy.deepcopy(df)
-            #     df_z = copy.deepcopy(df)
-            #     df_y["y"]=-df_y["y"]
-            #     df_y=df_y[(df_y["z"] > 0)&(df_y["y"] < 0)]
-            #     #df_y.to_csv(dir_name + "/moleculer_fieldtesty.csv")
-            #     df_z = df_z[(df_z["y"] !=0) & (df_z["z"] > 0) ]
-            #     df_z["z"]=-df_z["z"]
-            #     #df_z.to_csv(dir_name + "/moleculer_fieldtestz.csv")
-            #     df_z[feature] = -df_z[feature]
-            #     #df_z.to_csv(dir_name + "/moleculer_fieldtestz.csv")
-            #     df_yz=copy.deepcopy(df)
-            #     df_yz["y"] = -df_yz["y"]
-            #     df_yz["z"] = -df_yz["z"]
-            #     df_z0 = copy.deepcopy(df[df["z"]==1])
-            #     df_z0[feature]=0
-            #     df_z0["z"]=0
-            #     #df_z0.to_csv(dir_name + "/moleculer_fieldtestz0.csv")
-            #     #df_z0=df_z0[df_z0["y"]!=0]
-            #     df_z01=copy.deepcopy(df_z0)
-            #     df_z01["y"]=-df_z01["y"]
-            #     df_z01 = df_z01[df_z01["y"] != 0]
-            #     #df_z01.to_csv(dir_name + "/moleculer_fieldtestyz.csv")
-            #     df_yz1 = copy.deepcopy(df_yz)
-            #     # print(df_yz)
-            #     # print(df_yz1)
-            #     df_yz.to_csv(dir_name + "/moleculer_fieldtestyz1.csv")
-            #     df_yz["y"]=df_yz1[df_yz1["z"]<=0]["y"]
-            #     df_yz[feature]=-df_yz[feature]
-            #     #df_yz.to_csv(dir_name + "/moleculer_fieldtestyz.csv")
-            #     df=pd.concat([df_z0,df_z01,df,df_y,df_z,df_yz]).sort_values(by=["x","y","z"])
-            #
-            # else:
-            #     None
+
             df.sort_values(by=["x", "y", "z"])
             df.to_csv(dir_name + "/moleculer_fieldunfold.csv")
 
