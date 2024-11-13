cef5bdce53b2187f4d0a5d2b8206c3262b7be22d
John Doe <sakaguchi-daimon-nh@ynu.jp>
2024-11-13 20:21:54 +0900

    dataset_cleaning

--- CoMFA_model_lib/make_dataset.py
+++ CoMFA_model_lib/make_dataset.py
@@ -6,10 +6,11 @@ from rdkit.Chem import PandasTools
 from sklearn.model_selection import train_test_split
 
 def common(from_file_path):
-    df = pd.read_excel(from_file_path, engine="openpyxl").dropna(subset=["smiles"])
-    df["mol"] = df["smiles"].apply(Chem.MolFromSmiles)
-    df = df.dropna(subset=['er.', "mol", "smiles"])
-    df["inchikey"] = df["mol"].apply(lambda mol: Chem.inchi.MolToInchiKey(Chem.AddHs(mol)))
+    df = pd.read_excel(from_file_path, engine="openpyxl")
+    df["SMILES"]=df["SMILES"].apply(lambda smiles: Chem.MolToSmiles(Chem.MolFromSmiles(smiles)))
+    df["mol"] = df["SMILES"].apply(Chem.MolFromSmiles)
+    df = df.dropna(subset=['er.', "mol", "SMILES"])
+    df["InChIKey"] = df["mol"].apply(lambda mol: Chem.inchi.MolToInchiKey(Chem.AddHs(mol)))
     df["er."]=df["er."].apply(lambda x:np.clip(x,0.25,99.75))
     df = df[df["mol"].map(lambda mol: not mol.HasSubstructMatch(Chem.MolFromSmarts("[I]")))]
     df["RT"] = 1.99 * 10 ** -3 * df["temperature"].values
@@ -22,9 +23,8 @@ def output(df,to_file_path):
     test_df['test'] = 1
     df = pd.concat([train_df, test_df])
 
-    PandasTools.AddMoleculeColumnToFrame(df, "smiles")
-    df = df[["entry","smiles", "ROMol", "inchikey", "er.", "RT", "ΔΔG.expt.","Reference url","test"]].drop_duplicates(
-        subset="inchikey")
+    PandasTools.AddMoleculeColumnToFrame(df, "SMILES")
+    df = df[["entry","SMILES", "ROMol", "InChIKey", "er.", "RT", "ΔΔG.expt.","Reference url","test"]]
     PandasTools.SaveXlsxFromFrame(df, to_file_path, size=(100, 100))
 
     df["aliphatic_aliphatic"]=df["ROMol"].map(lambda mol: mol.HasSubstructMatch(Chem.MolFromSmarts("CC(=O)C")))
@@ -35,20 +35,20 @@ def output(df,to_file_path):
     print(len(df[df["aliphatic_aliphatic"]&~df["ring"]&~df["test"]]),len(df[df["aliphatic_aliphatic"]&~df["ring"]&df["test"]]),
           len(df[df["aliphatic_aromatic"]&~df["ring"]&~df["test"]]),len(df[df["aliphatic_aromatic"]&~df["ring"]&df["test"]]),
           len(df[df["aromatic_aromatic"]&~df["ring"]&~df["test"]]),len(df[df["aromatic_aromatic"]&~df["ring"]&df["test"]]),
-          len(df[df["ring"]&~df["test"]]),len(df[df["ring"]&df["test"]]))
+          len(df[df["ring"]&~df["test"]]),len(df[df["ring"]&df["test"]]),
+          len(df),len(df[df["test"]==0]),len(df[df["test"]==1]))
 
 if __name__ == '__main__':
-
     
-    df_cbs=common("C:/Users/poclabws/PycharmProjects/CoMFA_model/sampledata/CBS.xlsx")
-    df_dip=common("C:/Users/poclabws/PycharmProjects/CoMFA_model/sampledata/DIP.xlsx")
-    df_ru=common("C:/Users/poclabws/PycharmProjects/CoMFA_model/sampledata/Ru.xlsx")
+    df_cbs=common("C:/Users/poclabws/PycharmProjects/CoMFA_model/sampledata/CBS.xlsx")#.drop_duplicates(subset="InChIKey")
+    df_dip=common("C:/Users/poclabws/PycharmProjects/CoMFA_model/sampledata/DIP.xlsx")#.drop_duplicates(subset="InChIKey")
+    df_ru=common("C:/Users/poclabws/PycharmProjects/CoMFA_model/sampledata/Ru.xlsx")#.drop_duplicates(subset="InChIKey")
     
-    to_dir_path = "C:/Users/poclabws/PycharmProjects/CoMFA_model/all_dataset"
-    os.makedirs(to_dir_path, exist_ok=True)
-    output(df_cbs,f'{to_dir_path}/CBS.xlsx')
-    output(df_dip,f'{to_dir_path}/DIP.xlsx')
-    output(df_ru,f'{to_dir_path}/Ru.xlsx')
+    # to_dir_path = "C:/Users/poclabws/PycharmProjects/CoMFA_model/all_dataset"
+    # os.makedirs(to_dir_path, exist_ok=True)
+    # output(df_cbs,f'{to_dir_path}/CBS.xlsx')
+    # output(df_dip,f'{to_dir_path}/DIP.xlsx')
+    # output(df_ru,f'{to_dir_path}/Ru.xlsx')
 
     df_cbs = df_cbs[df_cbs["mol"].map(lambda mol: not mol.HasSubstructMatch(Chem.MolFromSmarts("n")))]
     df_dip=df = df_dip[df_dip["mol"].map(lambda mol:
@@ -57,7 +57,13 @@ if __name__ == '__main__':
                               and not mol.HasSubstructMatch(Chem.MolFromSmarts("[#6]C(=O)[#6]**[#7,OH1]")))]
     
     to_dir_path = "C:/Users/poclabws/PycharmProjects/CoMFA_model/arranged_dataset"
+
     os.makedirs(to_dir_path, exist_ok=True)
     output(df_cbs,f'{to_dir_path}/CBS.xlsx')
     output(df_dip,f'{to_dir_path}/DIP.xlsx')
     output(df_ru,f'{to_dir_path}/Ru.xlsx')
+    df_all=pd.concat([df_cbs,df_dip,df_ru])[["InChIKey","SMILES"]]
+    print(len(df_all))
+    df_all=df_all.drop_duplicates(subset=["InChIKey"])
+    print(len(df_all))
+    df_all.to_csv(f'{to_dir_path}/mol_list.csv',index=False)
