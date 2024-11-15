97f48b9d456b5e38b2ae887a3900adacf983593d
shimonomasakiynu <shimonomasakiynu@gmail.comgit config --global user.email shimonomasakiynu@gmail.com>
2024-11-15 20:45:36 +0900

    20241115ver

--- CoMFA_model_lib/graph_.py
+++ CoMFA_model_lib/graph_.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+
+def graph_(path):
+    df=pd.read_pickle(path)
+    columns=df.columns[df.columns.str.contains('cv ElasticNet')]
+    print(columns)
+    rmses=[]
+    for column in columns:
+        rmse=mean_squared_error(df["ΔΔG.expt."], df.loc[:,column],squared=False)
+        rmses.append(rmse)
+    print(rmses)
+    column=columns[rmses.index(min(rmses))]
+    plt.scatter(df["ΔΔG.expt."],df[column])
+
+    path=path.replace(".pkl",".png")
+    plt.savefig(path)
 
 if __name__ == '__main__':
     path="/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/cbs_regression.pkl"
-    df=pd.read_pickle(path)
-    plt.scatter(df["ΔΔG.expt."],df["predict 1 1"])
-    path=path.replace(".pkl",".png")
-    plt.savefig(path)
+    graph_(path)
