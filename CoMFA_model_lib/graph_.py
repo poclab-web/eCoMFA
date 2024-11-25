fe6ccdcc0f63443c801dec66f73b48994831f3bb
shimonomasakiynu <shimonomasakiynu@gmail.comgit config --global user.email shimonomasakiynu@gmail.com>
2024-11-25 15:17:29 +0900

    20241125

--- CoMFA_model_lib/graph_.py
+++ CoMFA_model_lib/graph_.py
@@ -1,22 +1,123 @@
+from itertools import product
+import os
+import re
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
+from rdkit.Chem import PandasTools
+import time
 
-def graph_(path):
+def nan_rmse(x,y):
+    return np.sqrt(np.nanmean((y-x)**2))
+
+def nan_r2(x,y):
+    x,y=x[~np.isnan(x)],y[~np.isnan(x)]
+    return 1-np.sum((y-x)**2)/np.sum((y-np.mean(y))**2)
+
+def best_parameter(path):
+    #値を丸める
     df=pd.read_pickle(path)
-    columns=df.columns[df.columns.str.contains('cv ElasticNet')]
-    print(columns)
-    rmses=[]
-    for column in columns:
-        rmse=mean_squared_error(df["ΔΔG.expt."], df.loc[:,column],squared=False)
-        rmses.append(rmse)
-    print(rmses)
-    column=columns[rmses.index(min(rmses))]
-    plt.scatter(df["ΔΔG.expt."],df[column])
+    cv_columns=df.filter(like='ElasticNet cv').columns
+    rmses=[nan_rmse(df[column].values,df["ΔΔG.expt."].values) for column in cv_columns]
+    best_cv_column=cv_columns[rmses.index(min(rmses))]
+    coef=pd.read_csv(path.replace(".pkl",".csv"), index_col=0)
+    coef = coef[[best_cv_column.replace("cv", "steric_coef"), best_cv_column.replace("cv", "electrostatic_coef")]]
+    coef.columns = ["steric_coef", "electrostatic_coef"]
 
-    path=path.replace(".pkl",".png")
-    plt.savefig(path)
+    start=time.time()
+    columns=[]
+    data=[]
+    for column in df.filter(like='steric_unfold').columns:
+        x,y,z=map(int, re.findall(r'[+-]?\d+', column))
+        columns.append(column.replace("unfold","cont"))
+        data.append(df[column]*coef.at[f'{x} {abs(y)} {abs(z)}',"steric_coef"]*np.sign(z))
+    data=pd.DataFrame(data=np.array(data).T,columns=columns)
+    data["steric_cont"]=data.sum(axis=1)
+    df=pd.concat([df,data],axis=1)
+    print(time.time()-start)
+    
+    # df["steric_cont"]=np.sum(df[[column.replace("unfold","cont")for column in df.filter(like='steric_unfold')]].values,axis=1)
+    df["cv"]=df[best_cv_column]
+    prediction_column=best_cv_column.replace("cv","prediction")
+    df["prediction"]=df[prediction_column]
+    regression_column=best_cv_column.replace("cv","regression")
+    df["regression"]=df[regression_column]
+    df["cv_error"]=df["cv"]-df["ΔΔG.expt."]
+    df["prediction_error"]=df["prediction"]-df["ΔΔG.expt."]
+    # df = df.reindex(df[["prediction_error","cv_error"]].abs().sort_values(ascending=False).index)
+    
+    df_=df[["SMILES","InChIKey","ΔΔG.expt.","steric_cont","regression","prediction","cv","prediction_error","cv_error"]].sort_values(["cv_error","prediction_error"]).fillna("NAN")
+    PandasTools.AddMoleculeColumnToFrame(df_, "SMILES")
+    path=path.replace(".pkl",".xlsx")
+    PandasTools.SaveXlsxFromFrame(df_,path, size=(100, 100))
+    return df#[["ΔΔG.expt.","regression","prediction","cv"]]
+
+def make_cube(df,path):
+    print(df.filter(like='steric_cont').columns)
+    grid = np.array([re.findall(r'[+-]?\d+', col) for col in df.filter(like='steric_cont ').columns]).astype(int)
+    min=np.min(grid,axis=0)
+    max=np.max(grid,axis=0)
+    rang=max-min+np.array([1,1,1])
+    columns=["InChIKey"]
+    for x,y,z in product(range(min[0],max[0]+1),range(min[1],max[1]+1),range(min[2],max[2]+1)):
+        columns.append(f'steric_cont {x} {y} {z}')
+        
+    df=df.reindex(columns=columns, fill_value=0)
+    min=' '.join(map(str, min))
+    for inchikey,value in zip(df["InChIKey"],df.iloc[:,1:].values):
+        dt=f'/Volumes/SSD-PSM960U3-UW/CoMFA_calc/{inchikey}/Dt0.cube'
+        with open(dt, 'r', encoding='UTF-8') as f:
+            f.readline()
+            f.readline()
+            n_atom,x,y,z=f.readline().split()
+            n_atom=int(n_atom)
+            f.readline()
+            f.readline()
+            f.readline()
+            coord=[f.readline() for _ in range(n_atom)]
+            # dt=f.read().splitlines()[2:]
+        # n_atom=int(dt[0].split()[0])
+        # coord=dt[4:n_atom+4]
+        coord=''.join(coord)
+        value='\n'.join([' '.join(f"{x}" for x in value[i:i + 6])for i in range(0, len(value), 6)])
+        os.makedirs(f'{path}/{inchikey}',exist_ok=True)
+        with open(f'{path}/{inchikey}/steric.cube','w') as f:
+            print(f'contribution Gaussian Cube File.\nsteric\n{n_atom} {min}\n{rang[0]} 1 0 0\n{rang[1]} 0 1 0\n{rang[2]} 0 0 1\n{coord}\n{value}',file=f)
+
+
+def graph_(df,path):
+    #r2,rmse表示
+    #直線表示
+    #軸ラベル表示
+    # データ数表示
+    #グリッド寄与度→cube
+    
+    plt.figure(figsize=(3, 3))
+    plt.yticks([-4,0,4])
+    plt.xticks([-4,0,4])
+    plt.ylim(-4,4)
+    plt.xlim(-4,4)
+    
+    plt.scatter(df["ΔΔG.expt."],df["regression"],c="black",linewidths=0,s=10,alpha=0.5)
+    rmse=nan_rmse(df["prediction"].values,df["ΔΔG.expt."].values)
+    r2=nan_r2(df["prediction"].values,df["ΔΔG.expt."].values)
+    plt.scatter([],[],label="$\mathrm{RMSE_{regression}}$"+f" = {rmse:.2f}"
+                   +"\n$r^2_{regression}$ = " + f"{r2:.2f}",c="black",linewidths=0,  alpha=0.6, s=10)
+    plt.scatter(df["ΔΔG.expt."],df["cv"],c="blue",linewidths=0,s=10,alpha=0.5)
+    plt.scatter(df["ΔΔG.expt."],df["prediction"],c="red",linewidths=0,s=10,alpha=0.5)
+    plt.xlabel("ΔΔ$\mathit{G}_{\mathrm{expt}}$ [kcal/mol]")
+    plt.ylabel("ΔΔ$\mathit{G}_{\mathrm{predict}}$ [kcal/mol]")
+    plt.legend(loc='lower right', fontsize=5, ncols=1)
+    plt.tight_layout()
+    plt.savefig(path.replace(".pkl",".png"),dpi=500,transparent=True)
+    # df = df.reindex(df["error"].abs().sort_values(ascending=False).index)
 
 if __name__ == '__main__':
-    path="/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/cbs_regression.pkl"
-    graph_(path)
+    df_cbs=best_parameter("/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/cbs_regression.pkl")
+    df_dip=best_parameter("/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/DIP_regression.pkl")
+    df_ru=best_parameter("/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/Ru_regression.pkl")
+    make_cube(df_cbs,'/Volumes/SSD-PSM960U3-UW/CoMFA_results/CBS')
+    make_cube(df_dip,'/Volumes/SSD-PSM960U3-UW/CoMFA_results/DIP')
+    make_cube(df_ru,'/Volumes/SSD-PSM960U3-UW/CoMFA_results/Ru')
+    df=pd.concat([df_cbs,df_dip,df_ru])
+    graph_(df,"/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/regression.png")
--- CoMFA_model_lib/regression.py
+++ CoMFA_model_lib/regression.py
@@ -4,84 +4,86 @@ import numpy as np
 import pandas as pd
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.linear_model import ElasticNet
-from rdkit.Chem import PandasTools
 from sklearn.model_selection import KFold
 
-def regression_ElasticNet(X_train,X_test,y_train,alpha,l1ratio):
+def regression_ElasticNet(X_train,X_test,y_train,y,alpha,l1ratio):
     model=ElasticNet(alpha=alpha,l1_ratio=l1ratio, fit_intercept=False)
     model.fit(X_train, y_train)
     coef=model.coef_
     predict=model.predict(X_test)
+    predict=np.clip(predict, np.min(y), np.max(y))
     return coef,predict
 
-def regression_PLS(X_train,X_test,y_train,n_components):
+def regression_PLS(X_train,X_test,y_train,y,n_components):
     model = PLSRegression(n_components=n_components)
     model.fit(X_train, y_train)
-    coef=model.coef_
-    predict=model.predict(X_test)
+    coef=model.coef_[0]
+    predict=model.predict(X_test)[:,0]
+    predict=np.clip(predict, np.min(y), np.max(y))
     return coef,predict
 
 def regression_(path):
     df=pd.read_pickle(path).sort_values(by="test")
-    df_train,df_test=df[df["test"]==0],df[df["test"]==1]
-
-    steric_train = df_train.filter(like='steric fold').to_numpy()
-    steric_test = df_test.filter(like='steric fold').to_numpy()
-    electrostatic_train = df_train.filter(like='electrostatic fold').to_numpy()
-    electrostatic_test = df_test.filter(like='electrostatic fold').to_numpy()
+    df_train=df[df["test"]==0]
+    steric_train = df_train.filter(like='steric_fold').to_numpy()
+    steric = df.filter(like='steric_fold').to_numpy()
+    electrostatic_train = df_train.filter(like='electrostatic_fold').to_numpy()
+    electrostatic = df.filter(like='electrostatic_fold').to_numpy()
     
     steric_std,electrostatic_std=np.linalg.norm(steric_train),np.linalg.norm(electrostatic_train)
     steric_train/=steric_std
-    steric_test/=steric_std
+    steric/=steric_std
     electrostatic_train/=electrostatic_std
-    electrostatic_test/=electrostatic_std
+    electrostatic/=electrostatic_std
     
-    X_train,X_test=np.concatenate([steric_train,electrostatic_train],axis=1),np.concatenate([steric_test,electrostatic_test],axis=1)
-    y_train,y_test=df_train["ΔΔG.expt."].values,df_test["ΔΔG.expt."].values
-    grid = [re.findall(r'\d+', col) for col in df.columns[df.columns.str.contains('steric fold')]]
-    grid=pd.DataFrame(data=grid,columns=["x", "y", "z"])
+    X_train,X=np.concatenate([steric_train,electrostatic_train],axis=1),np.concatenate([steric,electrostatic],axis=1)
+    y_train,y=df_train["ΔΔG.expt."].values,df["ΔΔG.expt."].values
+    grid=pd.DataFrame(index=[col.replace("steric_fold ","") for col in df.filter(like='steric_fold ').columns])
+    for alpha,l1ratio in product(np.logspace(-6,-3,4,base=10),np.round(np.linspace(0, 1, 3),decimals=10)):
+        coef,predict=regression_ElasticNet(X_train,X,y_train,y,alpha,l1ratio)
+        grid[f"ElasticNet steric_coef {alpha} {l1ratio}"]=coef[:len(coef) // 2]/steric_std
+        grid[f"ElasticNet electrostatic_coef {alpha} {l1ratio}"]=coef[len(coef) // 2:]/electrostatic_std
+        df[f'ElasticNet regression {alpha} {l1ratio}'] = np.where(df["test"] == 0, predict, np.nan)
+        df[f'ElasticNet prediction {alpha} {l1ratio}'] = np.where(df["test"] == 1, predict, np.nan)
 
-    for alpha,l1ratio in product(np.logspace(-6,0,10,base=10),np.linspace(0, 1, 11)):
-        coef,predict=regression_ElasticNet(X_train,np.concatenate([X_train,X_test],axis=0),y_train,alpha,l1ratio)
-        grid[f"ElasticNet steric {alpha} {l1ratio}"],grid[f"ElasitNet electrostatic {alpha} {l1ratio}"]=coef[:len(coef) // 2],coef[len(coef) // 2:]
-        df[f'predict ElasticNet {alpha} {l1ratio}']=predict
         cvs=[]
         sort_index=[]
         kf = KFold(n_splits=5, shuffle=True, random_state=1)
         for train_index, test_index in kf.split(y_train):
-            _,cv=regression_ElasticNet(X_train[train_index],X_train[test_index],y_train[train_index],alpha,l1ratio)
+            _,cv=regression_ElasticNet(X_train[train_index],X_train[test_index],y_train[train_index],y,alpha,l1ratio)
             cvs.extend(cv)
             sort_index.extend(test_index)
         
         original_array = np.empty_like(cvs)
         original_array[sort_index] = cvs
-        df[f'cv ElasticNet {alpha} {l1ratio}']=np.concatenate([original_array,np.zeros_like(y_test)])
+        
+        df.loc[df["test"]==0,f'ElasticNet cv {alpha} {l1ratio}']=original_array
 
     #PLS
     for n_components in range(1,10):
-        coef,predict=regression_PLS(X_train,X_test,y_train,n_components)
-        grid[f"PLS steric {n_components}"],grid[f"PLS electrostatic {n_components}"]=coef[:len(coef) // 2],coef[len(coef) // 2:]
-        df[f'predict PLS {alpha} {l1ratio}']=predict
+        coef,predict=regression_PLS(X_train,X,y_train,y,n_components)
+        grid[f"PLS steric_coef {n_components}"],grid[f"PLS electrostatic_coef {n_components}"]=coef[:len(coef) // 2],coef[len(coef) // 2:]
+        df[f'PLS regression {n_components}'] = np.where(df["test"] == 0, predict, np.nan)
+        df[f'PLS prediction {n_components}'] = np.where(df["test"] == 1, predict, np.nan)
         cvs=[]
         sort_index=[]
         kf = KFold(n_splits=5, shuffle=True, random_state=1)
         for train_index, test_index in kf.split(y_train):
-            _,cv=regression_PLS(X_train[train_index],X_train[test_index],y_train[train_index],n_components)
+            _,cv=regression_PLS(X_train[train_index],X_train[test_index],y_train[train_index],y,n_components)
             cvs.extend(cv)
             sort_index.extend(test_index)
         
         original_array = np.empty_like(cvs)
         original_array[sort_index] = cvs
-        df[f'cv PLS {n_components}']=np.concatenate([original_array,np.zeros_like(y_test)])
+        df.loc[df["test"]==0,f'PLS cv {n_components}']=original_array
 
     path=path.replace(".pkl","_regression.pkl")
     df.to_pickle(path)
     path=path.replace(".pkl",".csv")
     grid.to_csv(path)
+    print(grid.index)
     
-    # PandasTools.AddMoleculeColumnToFrame(df, "SMILES")
-    # PandasTools.SaveXlsxFromFrame(df, path, size=(100, 100))
-
 if __name__ == '__main__':
-    path="/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/cbs.pkl"
-    regression_(path)
+    regression_("/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/cbs.pkl")
+    regression_("/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/DIP.pkl")
+    regression_("/Users/mac_poclab/PycharmProjects/CoMFA_model/arranged_dataset/Ru.pkl")
